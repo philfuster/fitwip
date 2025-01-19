46a70eb6a862ca713bfaa27007049f91d7e0585c
Phil Fuster <pafus23@protonmail.com>
2025-01-19 00:12:48 -0500

    Add Emotion dependency and set up global styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,46 @@
+import styled from "@emotion/styled";
 import "./App.css";
 import { TestComponent } from "./shared/components";
+import { Global } from "@emotion/react";
+
+const AppContainer = styled("div")({ width: "100%" });
 
 function App() {
 	return (
 		<>
-			<TestComponent />
-			<p>Hey</p>
-			<TestComponent />
+			<Global
+				styles={{
+					html: {
+						boxSizing: "border-box",
+					},
+					"*, *:before, *:after": {
+						boxSizing: "inherit",
+					},
+					body: {
+						margin: 0,
+						lineHeight: "1.2",
+						overflowX: "hidden",
+						WebkitFontSmoothing: "antialiased", // Antialisasing
+						MoxOsFontSmoothing: "grayscale", // Antialisasing
+					},
+					a: {
+						textDecoration: "none",
+					},
+					"a:hover": {
+						textDecoration: "underline",
+					},
+					img: {
+						maxWidth: "100%",
+						height: "auto",
+						width: "auto",
+					},
+				}}
+			/>
+			<AppContainer>
+				<TestComponent />
+				<p>Hey</p>
+				<TestComponent />
+			</AppContainer>
 		</>
 	);
 }
