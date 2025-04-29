e4b13952832f3a2ae797d3b891a771fec3b83383
Phil Fuster <pafus23@protonmail.com>
2025-04-29 01:34:03 -0400

    Update global styles define global styles outside of App component so the styles aren't re-created on every render

--- src/pages/App/App.tsx
+++ src/pages/App/App.tsx
@@ -1,40 +1,41 @@
 import styled from "@emotion/styled";
 import { TestComponent } from "../../shared/components";
-import { Global } from "@emotion/react";
+import { Global, css } from "@emotion/react";
 
-const AppContainer = styled("div")({ width: "100%" });
+const AppContainer = styled("div")();
+
+// Define global styles outside the component to avoid re-creation on every render
+const globalStyles = css({
+	html: {
+		boxSizing: "border-box",
+	},
+	"*, *:before, *:after": {
+		boxSizing: "inherit",
+	},
+	body: {
+		margin: 0,
+		lineHeight: "1.2",
+		overflowX: "hidden",
+		WebkitFontSmoothing: "antialiased", // Antialiasing
+		MoxOsFontSmoothing: "grayscale", // Antialiasing
+	},
+	a: {
+		textDecoration: "none",
+	},
+	"a:hover": {
+		textDecoration: "underline",
+	},
+	img: {
+		maxWidth: "100%",
+		height: "auto",
+		width: "auto",
+	},
+});
 
 function App() {
 	return (
 		<>
-			<Global
-				styles={{
-					html: {
-						boxSizing: "border-box",
-					},
-					"*, *:before, *:after": {
-						boxSizing: "inherit",
-					},
-					body: {
-						margin: 0,
-						lineHeight: "1.2",
-						overflowX: "hidden",
-						WebkitFontSmoothing: "antialiased", // Antialisasing
-						MoxOsFontSmoothing: "grayscale", // Antialisasing
-					},
-					a: {
-						textDecoration: "none",
-					},
-					"a:hover": {
-						textDecoration: "underline",
-					},
-					img: {
-						maxWidth: "100%",
-						height: "auto",
-						width: "auto",
-					},
-				}}
-			/>
+			<Global styles={globalStyles} />
 			<AppContainer>
 				<TestComponent />
 				<p>Hey</p>
